b411a33e2d0afc07a218aa0530af27c8f7cff291
Tomohiro Sasaya <sasaya7@gmail.com>
2020-12-29 07:40:14 +0900

    ツイート機能の実装

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const roulette　= ["密", "蜜", "宓", "淧", "䀄"];
 const resultDivided = document.getElementById('result-area');
 const rouletteButton = document.getElementById('roulette');
 const textareaDivided = document.getElementById('text-area');
+const tweetDivided = document.getElementById('tweet-area');
 const stopButton = document.getElementById('stop');
 
 resultDivided.innerText = roulette[0] + roulette[0] + roulette[1];
 let roulette1;
 let result;
-
+/**
+ * 指定した要素の子どもを全て削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    // 子どもの要素があるかぎり削除
+    element.removeChild(element.firstChild);
+  }
+}
 //　ルーレットを開始
 function start() {
   roulette1  = setInterval(function() {
@@ -36,5 +46,24 @@ function stop() {
 stopButton.onclick = () => {
  stop();
  let count = (result.match( /密/g ) || [] ).length;
- textareaDivided.textContent = count + "密です。";
+ let result2 = count + "密です。";
+ textareaDivided.textContent = result2;
+ // TODO ツイートエリアの作成
+ removeAllChildren(tweetDivided);
+ const anchor = document.createElement('a');
+ const hrefValue =
+    'https://twitter.com/intent/tweet?button_hashtag=' + 
+    encodeURIComponent('三密ルーレット') + 
+    '&ref_src=twsrc%5Etfw';
+ anchor.setAttribute('href', hrefValue);
+ anchor.className = 'twitter-hashtag-button';
+ anchor.setAttribute('data-text', result + ' ' +result2);
+ anchor.innerText = 'Tweet #三密ルーレット';
+ tweetDivided.appendChild(anchor);
+
+ // widgets.js の設定
+ const script = document.createElement('script');
+ script.setAttribute('src','https://platform.twitter.com/widgets.js');
+ tweetDivided.appendChild(script);
+ 
 }
