307f3cde79e51c76e4bb655dfb13e98e8a39113e
Tomohiro Sasaya <sasaya7@gmail.com>
2021-03-31 10:33:58 +0900

    ツイート時にサイトのURLを投稿する

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const roulette　= ["密", "蜜", "宓", "淧", "䀄"];
+const roulette = ["密", "蜜", "宓", "淧", "䀄"];
 const resultDivided = document.getElementById('result-area');
 const rouletteButton = document.getElementById('roulette');
 const textareaDivided = document.getElementById('text-area');
@@ -10,7 +10,7 @@ resultDivided.innerText = roulette[0] + roulette[0] + roulette[1];
 let roulette1;
 let result;
 let a = false;
-/**
+/** 
  * 指定した要素の子どもを全て削除する
  * @param {HTMLElement} element HTMLの要素
  */
@@ -18,34 +18,63 @@ function removeAllChildren(element) {
   while (element.firstChild) {
     // 子どもの要素があるかぎり削除
     element.removeChild(element.firstChild);
-  }
-}
-//　ルーレットを開始
+  }}
+//ルーレット開始
 function start() {
   if(a === false){
-    a = true;
-    roulette1  = setInterval(function() {
-    let r1 = Math.floor( Math.random() * 100);
-    const i = r1 % roulette.length;
-    let r2 = Math.floor( Math.random() * 100);
-    const j = r2 % roulette.length;
-    let r3 = Math.floor( Math.random() * 100);
-    const k = r3 % roulette.length;
-    result = roulette[i] + roulette[j] + roulette[k];
-    resultDivided.innerText = result;
-    textareaDivided.textContent =　'';
-  } , 100); 
-}
+    roulette1 = setInterval(function() {
+      let r1 = Math.floor(Math.random() * 100);
+      const i = r1 % roulette.length;
+      let r2 = Math.floor(Math.random() * 100);
+      const j = r2 % roulette.length;
+      let r3 = Math.floor(Math.random() * 100);
+      const k = r3 % roulette.length;
+      result = roulette[i] + roulette[j] + roulette[k];
+      resultDivided.innerText = result;
+      textareaDivided.textContent = '';
+    }, 100);
+  }
 }
 rouletteButton.onclick = () => {
- start();
+  start();
 }
 
-// ルーレットを停止
+//ルーレットを停止
 function stop() {
   if(roulette1){
     clearInterval(roulette1);
-  }}
+  }
+}
+
+stopButton.onclick = () => {
+  a = false;
+  stop();
+  let n = (result.match(/密/g) ||[]).length;
+  let result2 = n + "密です。";
+  textareaDivided.textContent = result2;
+  //
+}
+// const endAfter3denses = async (msInterval) => {
+//   let denseNum = "0";
+//   const textareaElement = document.getElementById('text-area');
+//   const rouletteElement = document.getElementById('roulette');
+//   const stopElement = document.getElementById('stop');
+//   let pushedEscape = false;
+//   document.onkeydown = e => {
+//       if (e.key == 'Escape') pushedEscape = true;
+//   }
+// ​
+//   while (!pushedEscape) {
+//       rouletteElement.click();
+//       await new Promise(resolve => setTimeout(resolve, msInterval));
+//       stopElement.click();
+//       denseNum = textareaElement.innerText.slice(0,1);
+//       if (denseNum == '3') {
+//           break;
+//       }
+//   }
+// }
+
 
 stopButton.onclick = () => {
   a = false;
@@ -54,21 +83,23 @@ stopButton.onclick = () => {
  let result2 = n + "密です。";
  textareaDivided.textContent = result2;
  // TODO ツイートエリアの作成
- removeAllChildren(tweetDivided);
- const anchor = document.createElement('a');
- const hrefValue =
-    'https://twitter.com/intent/tweet?button_hashtag=' + 
-    encodeURIComponent('三密ルーレット') + 
-    '&ref_src=twsrc%5Etfw';
- anchor.setAttribute('href', hrefValue);
- anchor.className = 'twitter-hashtag-button';
- anchor.setAttribute('data-text', result + ' ' +result2);
- anchor.innerText = 'Tweet #三密ルーレット';
- tweetDivided.appendChild(anchor);
-
- // widgets.js の設定
- const script = document.createElement('script');
- script.setAttribute('src','https://platform.twitter.com/widgets.js');
- tweetDivided.appendChild(script);
+  // TODO ツイートエリアの作成
+  removeAllChildren(tweetDivided);
+  const anchor = document.createElement('a');
+  const hrefValue =
+     'https://twitter.com/intent/tweet?button_hashtag=' + 
+     encodeURIComponent('三密ルーレット') + 
+     '&ref_src=twsrc%5Etfw';
+  anchor.setAttribute('href', hrefValue);
+  let result3 = decodeURIComponent('https://sumup7.github.io/three-dense/three-dense.html');
+  anchor.className = 'twitter-hashtag-button';
+  anchor.setAttribute('data-text', result + ' ' +result2 + result3);
+  anchor.innerText = 'Tweet #三密ルーレット';
+  tweetDivided.appendChild(anchor);
  
-}
+  // widgets.js の設定
+  const script = document.createElement('script');
+  script.setAttribute('src','https://platform.twitter.com/widgets.js');
+  tweetDivided.appendChild(script);
+  
+ }
